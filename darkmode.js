7872abce2f1fe7db88ea10916a29bb406b9ab5b6
RaffiGolonda <150316086+RaFYWStud@users.noreply.github.com>
2025-04-13 22:28:19 +0800

    Memodifikasi pewarnaan css menjadi variabel agar kompatibel dengan sistem darkmode dan menambahkan javascript untuk toggle darkmode light mode

---
+++ darkmode.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const toggleDarkMode = document.getElementById("dark-mode-toggle");
+
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme === "dark") {
+        document.body.classList.add("dark-mode");
+    }
+
+    toggleDarkMode.addEventListener("click", () => {
+        document.body.classList.toggle("dark-mode");
+
+        if (document.body.classList.contains("dark-mode")) {
+            localStorage.setItem("theme", "dark");
+        } else {
+            localStorage.setItem("theme", "light");
+        }
+    })
+});
